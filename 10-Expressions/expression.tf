fab91617d696ce15b3e1c62987a4d3a99eec2922
Your Name <root@your.domain.com>
2021-07-27 11:30:09 +0000

    11-Locals

--- 10-Expressions/expression.tf
+++ 10-Expressions/expression.tf
@@ -1,13 +1,13 @@
-provider "aws" { 
-version = "3.10"
-region  = "us-east-1"
+provider "aws" {
+  version = "3.10"
+  region  = "us-east-1"
 }
 
 
-provider "aws" { 
-version = "3.10"
-region  = "us-west-1"
-alias   = "us-west-1"
+provider "aws" {
+  version = "3.10"
+  region  = "us-west-1"
+  alias   = "us-west-1"
 }
 
 variable "zones_east" {
@@ -22,35 +22,35 @@ variable "multi-region-deployment" {
   default = false
 }
 
-variable "project-name" { 
-  default = "Terraform-demo"
+variable "project-name" {
+  default = "Terraform-demo-AV"
 }
 
 
 
 resource "aws_instance" "east_frontend" {
-  ami           = "ami-0dba2cb6798deb6d8"
-  instance_type = "t2.micro"
+  ami               = "ami-0dba2cb6798deb6d8"
+  instance_type     = "t2.micro"
   availability_zone = var.zones_east[count.index]
   count             = 2
-  depends_on    = [ aws_instance.east_backend ]
+  depends_on        = [aws_instance.east_backend]
   lifecycle {
     create_before_destroy = true
   }
 
- tags = {
-   Name = "${join("-", list(var.project-name, "Frontend" ))}-${count.index + 1}"
- }
-  
+  tags = {
+    Name = "${join("-", list(var.project-name, "Frontend"))}-${count.index + 1}"
+  }
+
 }
 
 resource "aws_instance" "west_frontend" {
-  ami           = "ami-0e4035ae3f70c400f"
-  instance_type = "t2.micro"
+  ami               = "ami-0e4035ae3f70c400f"
+  instance_type     = "t2.micro"
   availability_zone = var.zones_west[count.index]
   count             = var.multi-region-deployment ? 2 : 0
-  depends_on    = [ aws_instance.west_backend ]
-  provider      = aws.us-west-1
+  depends_on        = [aws_instance.west_backend]
+  provider          = aws.us-west-1
   lifecycle {
     create_before_destroy = true
   }
@@ -58,29 +58,29 @@ resource "aws_instance" "west_frontend" {
 
 
 resource "aws_instance" "east_backend" {
-  ami           = "ami-0947d2ba12ee1ff75"
-  instance_type = "t2.micro"
+  ami               = "ami-0947d2ba12ee1ff75"
+  instance_type     = "t2.micro"
   availability_zone = var.zones_east[count.index]
   count             = 2
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 
 
- tags = {
-   Name = "${join("-", list(var.project-name, "Backend" ))}-${count.index + 1}"
- }
+  tags = {
+    Name = "${join("-", list(var.project-name, "Backend"))}-${count.index + 1}"
+  }
 }
 
 
 resource "aws_instance" "west_backend" {
-  ami           = "ami-0e4035ae3f70c400f"
-  instance_type = "t2.micro"
+  ami               = "ami-0e4035ae3f70c400f"
+  instance_type     = "t2.micro"
   availability_zone = var.zones_west[count.index]
   count             = var.multi-region-deployment ? 2 : 0
-  provider      = aws.us-west-1
+  provider          = aws.us-west-1
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 }
 
