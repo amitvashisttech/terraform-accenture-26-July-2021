80c8695ea0ddf2c6378d80563b0e3adebfee160a
Your Name <root@your.domain.com>
2021-07-28 08:49:35 +0000

    14-Modules

--- 14-Modules/exp2/outputs.tf
+++ 14-Modules/exp2/outputs.tf
@@ -1,6 +1,3 @@
-output "PublicIP_East_Frontend" {
-  #value = aws_instance.instance.*.public_ip
-  #value = "${aws_instances.aws_instance.instance.*.public_ip}"
-  value = "${module.frontend.aws_instance.instance.*.public_ip}"
-
+output "PublicIP_East_Frontend-2" {
+  value = "${module.frontend.PublicIP_East_Frontend}"
 }
